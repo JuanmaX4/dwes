5d0c294d4a19c90395beae74b3cb1cd34fc34105
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2021-10-14 18:09:53 +0200

    Medio 1.0

--- 05-10-2021/Medio.js
+++ 05-10-2021/Medio.js
@@ -8,3 +8,11 @@ const estudiante = { nombre: 'Jacek', notas: [ 5, 4, 3 ] };
 
 /*{ nombre: 'Ewa', notas: [ 3, 3, 3 ] },
 { nombre: 'Zygmunt', notas: [ 1, 2, 3 ] };*/
+
+function invert(o) {
+	return Object.fromEntries(
+		Object.entries(o).map(arr => arr.reverse())
+	);
+}
+invert({"z": "q", "w": "f"});
+
