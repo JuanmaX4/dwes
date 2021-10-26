ce91c84f13051b1aea0a64b4c56a5d3fa25dec91
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2021-10-26 20:05:38 +0200

    Update Final Debug con VSCODE

--- Debug con VSCODE/Ejercicios.js
+++ Debug con VSCODE/Ejercicios.js
@@ -11,13 +11,16 @@
 function calcula() {
     var num1 = 2;
     console.log('Numero 1:', num1);
-    var num2 = 6;
+    var num2 = 6; //aqui cambie el programa para probar el debugg
     console.log('Numero 2:', num2);
     var sum = num1 + num2;
     console.log('sum:', sum);
     console.log(num1 + ' + ' + num2 + ' = ' + sum);
 }
 calcula();
+
+// peticion del got en el que se promete una peticion al servidor github
+//si responde sale todo el html en consola de la pagina web y obviamente si no sale saldra el error
 
 const got = require('got');
 
