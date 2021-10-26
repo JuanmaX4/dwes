42c11cd0dd4964b5e4d26791a7983ff284eb798b
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2021-10-26 20:00:06 +0200

    Update Final Nodejs: Debug con VSCODE

---
+++ Debug con VSCODE/Ejercicios.js
@@ -0,0 +1,33 @@
+/**1. Usar la consola en medio de una ejecucíon
+2. Añadir variables y expresiones en el panel de debug
+3. Alterar el valor de una variable y continuar la ejecución
+4. Deshabilitar los puntos de ruptura
+5. Realizar una petición con node-fetch e inspeccionar la respuesta en el inspector
+6. Extraer los datos de un objeto de una librería
+• Propiedades
+• Métodos
+• Eventos */
+
+function calcula() {
+    var num1 = 2;
+    console.log('Numero 1:', num1);
+    var num2 = 6;
+    console.log('Numero 2:', num2);
+    var sum = num1 + num2;
+    console.log('sum:', sum);
+    console.log(num1 + ' + ' + num2 + ' = ' + sum);
+}
+calcula();
+
+const got = require('got');
+
+(async () => {
+    try {
+        const response = await got('https://www.github.com');
+        console.log(response.body);
+        //=> '<!doctype html> ...'
+    } catch (error) {
+        console.log(error.response.body);
+        //=> 'Internal server error ...'
+    }
+})();
