0d007b5dff960ed989670cca84fc7c720ab55ffc
JuanmaX4 <74423956+JuanmaX4@users.noreply.github.com>
2021-12-13 14:02:05 +0100

    Update Final 2.0

--- API seguridad/Ejercicio2.js
+++ API seguridad/Ejercicio2.js
@@ -1,57 +1,92 @@
-const express = require('express');
-const  bcrypt  =  require ( 'bcrypt' ) ;
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
 const app = express();
-const port = 4003;
-const hash = '$2b$10$xrzO2R3ob3NJM//IQgWNAuzeHl725PcHF067/kEfSVJBPnXnFtt0q';
-const hash2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjEyMzR9.ff8Jdw9ijlQn8vWyhUpMZLQpT1Ew88MKISOAQ8JzK2w';
 
-//console.log(hash);
-
-/**1. Realizar un middleware que valide el acceso a través de un token válido (desencriptar el token
-con bcrypt)
-• Se tomará por acceso válido si el mensaje original es I know your secret. */
-
-// Middlewa que verifica si el usuario es un administrador.
-function isAdmin(req, res, next) {
-  const { password } = req.headers
-
-  // Cargue el hash de su base de datos de contraseñas. 
-    if(bcrypt.compareSync(password, hash)){
-      app.get('/admin', (req, res) => {
-        res.status(200).send('Bienvenid@, administrador');
-      });    
-  }
-    else{
-        res.status(401).send(`Acceso restringido, por favor, incluya la palabra secreta en el parámetro 'password' en la cabera de la petición`);
-    };  
-}
-
-function isVip(req, res, next) {
-  const { password } = req.headers
-
-  // Cargue el hash de su base de datos de contraseñas. 
-    if(bcrypt.compareSync(password, hash2)){
-      app.get('/vip', (req, res) => {
-        res.status(200).send('Bienvenid@, usuario vip');
-      });    
-  }
-    else{
-        res.status(401).send(`Acceso restringido, por favor, incluya la palabra secreta en el parámetro 'password' en la cabera de la petición`);
-    };  
-}
-
-// Se agrega el middleware en la aplicación.
-app.use(isAdmin);
-
-app.use(isVip);
-
-// Ruta a la cual solo deben ingresar usuarios administradores.
-app.get('/public', (req, res) => {
-    res.status(200).send('Bienvenid@, disfrute del contenido');
+app.get("/public", (req , res) => {
+    res.json({
+        mensaje: "Acceso público que permitirá el acceso a invitados"
+    });
 });
 
 
-app.listen(port, () => {
-  console.log(`Server listeting on port ${port}`)
+    //primero en el postman tienes que hacer una peticion post para que se genere el token para
+
+app.post("/api/login", (req , res) => {
+    const user = {
+        id: 1,
+        nombre : "Henry",
+        email: "henry@email.com"
+    }
+
+    jwt.sign({user}, 'secretkey', {expiresIn: '32s'}, (err, token) => {
+        res.json({
+            token
+        });
+    });
+
 });
+
+
+app.post("/api/vip", verifyToken, (req , res) => {
+
+    jwt.verify(req.token, 'secretkey', (error, authData) => {
+        if(error){
+            res.sendStatus(403);
+        }else{
+            res.json({
+                    mensaje: "bienvenido usuario vip",
+                    authData
+                });
+        }
+    });
+});
+
+app.post("/api/admin", verifyToken, (req , res) => {
+
+    jwt.verify(req.token, 'secretkey', (error, authData) => {
+        if(error){
+            res.sendStatus(403);
+        }else{
+            res.json({
+                    mensaje: "bienvenido usuario admin",
+                    authData
+                });
+        }
+    });
+});
+
+
+
+        //luego aqui la misma peticion en el postman hacemos un post y en el header ponemos
+        //en la key authorization y en el value el token que se nos genero anteriormente con el login
+
+app.post("/api/posts", verifyToken, (req , res) => {
+
+    jwt.verify(req.token, 'secretkey', (error, authData) => {
+        if(error){
+            res.sendStatus(403);
+        }else{
+            res.json({
+                    mensaje: "Post fue creado",
+                    authData
+                });
+        }
+    });
+});
+
+// Authorization: Bearer <token>
+function verifyToken(req, res, next){
+     const bearerHeader =  req.headers['authorization'];
+
+     if(typeof bearerHeader !== 'undefined'){
+          const bearerToken = bearerHeader.split(" ")[1];
+          req.token  = bearerToken;
+          next();
+     }else{
+         res.sendStatus(403);
+     }
+}
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado en el puerto: 3000")});
